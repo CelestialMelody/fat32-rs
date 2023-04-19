d95fae7b0193c75a2dc1fababdb8a2bb13b41dd2
melody <CelestialMelodyGo@gmail.com>
2023-04-19 22:39:40 +0800

    feat: cache

--- src/dir.rs
+++ src/dir.rs
@@ -1,16 +1,22 @@
+use crate::block_cache::get_block_cache;
+use crate::block_cache::Cache;
 use crate::block_device::BlockDevice;
 use crate::bpb::BIOSParameterBlock;
 use crate::entry::Entry;
 use crate::entry::NameType;
 use crate::fat::ClusterChain;
 use crate::file::File;
+use crate::DIR_ENTRY_UNUSED;
+use crate::DOT;
+use crate::END_OF_CLUSTER;
+use crate::SPACE;
 
 use crate::BLOCK_SIZE;
 
 use alloc::sync::Arc;
 use core::fmt::Debug;
 
-use crate::{generate_checksum, get_len_of_lfn, get_lfn_index, is_illegal, sfn_or_lfn};
+use crate::{generate_checksum, get_lde_cnt, get_lfn_index, is_illegal, sfn_or_lfn};
 
 /// Define DirError
 #[derive(Debug, PartialEq, Clone, Copy, Eq)]
@@ -33,7 +39,7 @@ pub enum OpType {
 pub struct Dir<'a> {
     pub(crate) device: Arc<dyn BlockDevice>,
     pub(crate) bpb: &'a BIOSParameterBlock,
-    pub(crate) detail: Entry,
+    pub(crate) sde: Entry,
     pub(crate) fat: ClusterChain,
 }
 
@@ -58,25 +64,25 @@ impl<'a> Dir<'a> {
         self.create(file, OpType::File)
     }
 
-    /// Open File, Return File<T> Type
+    /// Open File, Return File Type
     pub fn open_file(&self, file: &str) -> Result<File<'a>, DirError> {
         if is_illegal(file) {
             return Err(DirError::IllegalChar);
         }
         match self.exist(file) {
             None => Err(DirError::NoMatchFile),
-            Some(di) => {
-                if di.is_file() {
+            Some(dir_entry) => {
+                if dir_entry.is_file() {
                     let fat = ClusterChain::new(
-                        di.first_cluster(),
+                        dir_entry.first_cluster(),
                         Arc::clone(&self.device),
                         self.bpb.fat1(),
                     );
                     Ok(File {
                         device: Arc::clone(&self.device),
                         bpb: self.bpb,
-                        dir_cluster: self.detail.first_cluster(),
-                        sde: di,
+                        dir_cluster: self.sde.first_cluster(),
+                        sde: dir_entry,
                         fat,
                     })
                 } else {
@@ -86,7 +92,7 @@ impl<'a> Dir<'a> {
         }
     }
 
-    /// Cd Dir, Return Dir<T> Type
+    /// Cd Dir, Return Dir Type
     pub fn cd(&self, dir: &str) -> Result<Dir<'a>, DirError> {
         if is_illegal(dir) {
             return Err(DirError::IllegalChar);
@@ -103,7 +109,7 @@ impl<'a> Dir<'a> {
                     Ok(Self {
                         device: Arc::clone(&self.device),
                         bpb: self.bpb,
-                        detail: di,
+                        sde: di,
                         fat,
                     })
                 } else {
@@ -123,7 +129,7 @@ impl<'a> Dir<'a> {
         }
     }
 
-    /// Check if file or dir is exist or not through DirIter<T>, Return Option Type
+    /// Check if file or dir is exist or not through DirIter, Return Option Type
     pub fn exist_iter(&self, iter: &mut DirIter, value: &str) -> Option<Entry> {
         match sfn_or_lfn(value) {
             NameType::SFN => iter.find(|d| d.sfn_equal(value)),
@@ -133,10 +139,14 @@ impl<'a> Dir<'a> {
 
     /// Find Long File Name Item, Return Option Type
     fn find_lfn(&self, iter: &mut DirIter, value: &str) -> Option<Entry> {
-        let count = get_len_of_lfn(value);
+        let count = get_lde_cnt(value);
+        // 最后一个 lde 对应于 value 的 index
         let mut index = get_lfn_index(value, count);
         let mut has_match = true;
 
+        // 倒着找？
+
+        // TODO 直接last?
         let result = iter.find(|d| {
             if d.is_lfn()
                 && d.lde_order().unwrap() == count
@@ -184,7 +194,7 @@ impl<'a> Dir<'a> {
         }
 
         let blank_cluster = self.fat.blank_cluster();
-        self.fat.write(blank_cluster, 0x0FFFFFFF);
+        self.fat.write(blank_cluster, END_OF_CLUSTER);
 
         match sfn_or_lfn(value) {
             NameType::SFN => {
@@ -192,9 +202,11 @@ impl<'a> Dir<'a> {
                 self.write_directory_item(di, NameType::SFN);
             }
             NameType::LFN => {
+                // TODO 长文件名转短文件名
                 let sfn = "unsupported".as_bytes();
                 let check_sum = generate_checksum(sfn);
-                let count = get_len_of_lfn(value);
+                let count = get_lde_cnt(value);
+                // 最后一个 lde 对应于 value 的 index
                 let mut lfn_index = get_lfn_index(value, count);
 
                 let di =
@@ -253,7 +265,7 @@ impl<'a> Dir<'a> {
                 iter.update();
             }
             NameType::LFN => {
-                let count = get_len_of_lfn(name);
+                let count = get_lde_cnt(name);
                 for _ in 0..=count {
                     iter.previous();
                     iter.set_deleted();
@@ -300,6 +312,7 @@ impl<'a> Dir<'a> {
                 di_bytes.copy_from_slice(&di.lde_to_bytes_array().unwrap());
             }
         }
+        iter.update_item(&di_bytes);
         iter.update();
     }
 
@@ -318,16 +331,26 @@ impl<'a> Dir<'a> {
     fn add_dot_item(&self, cluster: u32) {
         let mut buffer = [0; BLOCK_SIZE];
 
-        let mut value = [0x20; 11];
+        let mut value = [SPACE; 11];
         value[0] = b'.';
         let mut di = Entry::new_sfn_bytes(cluster, &value, OpType::Dir);
         buffer[0..32].copy_from_slice(&di.sde_to_bytes_array().unwrap());
         value[1] = b'.';
-        di = Entry::new_sfn_bytes(self.detail.first_cluster(), &value, OpType::Dir);
+        di = Entry::new_sfn_bytes(self.sde.first_cluster(), &value, OpType::Dir);
         buffer[32..64].copy_from_slice(&di.sde_to_bytes_array().unwrap());
 
         let offset = self.bpb.offset(cluster);
-        self.device.write_blocks(&buffer, offset, 1).unwrap();
+        let block_id = offset / BLOCK_SIZE;
+        assert!(offset % BLOCK_SIZE == 0);
+
+        let option = get_block_cache(block_id, Arc::clone(&self.device));
+        if let Some(cache) = option {
+            cache.write().modify(0, |cache: &mut [u8; BLOCK_SIZE]| {
+                cache.copy_from_slice(&buffer);
+            })
+        } else {
+            self.device.write_blocks(&buffer, offset, 1).unwrap();
+        }
     }
 }
 
@@ -337,9 +360,9 @@ pub struct DirIter<'a> {
     device: Arc<dyn BlockDevice>,
     fat: ClusterChain,
     bpb: &'a BIOSParameterBlock,
-    offset: usize,
-    sector_offset: usize,
-    index: usize,
+    cluster_offset: usize,
+    sector_id_in_cluster: usize,
+    index_in_buf: usize,
     buffer: [u8; BLOCK_SIZE],
 }
 
@@ -356,59 +379,60 @@ impl<'a> DirIter<'a> {
             device: Arc::clone(&device),
             fat: fat.clone(),
             bpb,
-            offset: bpb.offset(fat.current_cluster),
-            sector_offset: 0,
-            index: 0,
+            cluster_offset: bpb.offset(fat.current_cluster),
+            sector_id_in_cluster: 0,
+            index_in_buf: 0,
             buffer: [0; BLOCK_SIZE],
         }
     }
 
     fn offset_value(&self) -> usize {
-        self.offset + self.sector_offset * BLOCK_SIZE
+        self.cluster_offset + self.sector_id_in_cluster * BLOCK_SIZE
     }
 
     fn offset_index(&mut self) {
         let spc = self.bpb.sector_per_cluster_usize();
 
-        self.index += 32;
-        if self.index % BLOCK_SIZE == 0 {
-            self.sector_offset += 1;
-            self.index = 0;
+        self.index_in_buf += 32;
+        if self.index_in_buf % BLOCK_SIZE == 0 {
+            self.sector_id_in_cluster += 1;
+            self.index_in_buf = 0;
         }
 
-        if self.sector_offset % spc == 0 && self.sector_offset != 0 {
+        if self.sector_id_in_cluster % spc == 0 && self.sector_id_in_cluster != 0 {
             if self.fat.next_is_none() {
-                self.sector_offset = spc;
+                self.sector_id_in_cluster = spc;
             } else {
                 self.fat.next();
-                self.offset = self.bpb.offset(self.fat.current_cluster);
-                self.sector_offset = 0;
+                self.cluster_offset = self.bpb.offset(self.fat.current_cluster);
+                self.sector_id_in_cluster = 0;
             }
         }
     }
 
     fn is_end_sector(&self) -> bool {
         let spc = self.bpb.sector_per_cluster_usize();
-        self.sector_offset == spc
+        self.sector_id_in_cluster == spc
     }
 
     fn is_end(&self) -> bool {
-        self.is_end_sector() || self.buffer[self.index] == 0x00
+        self.is_end_sector() || self.buffer[self.index_in_buf] == 0x00
     }
 
     fn is_special_item(&self) -> bool {
-        self.buffer[self.index] == 0x2E && self.buffer[self.index + 1] == 0x20
-            || self.buffer[self.index] == 0x2E
-                && self.buffer[self.index + 1] == 0x2E
-                && self.buffer[self.index + 2] == 0x20
+        // '.' or '..'
+        (self.buffer[self.index_in_buf] == DOT && self.buffer[self.index_in_buf + 1] == SPACE)
+            || (self.buffer[self.index_in_buf] == DOT
+                && self.buffer[self.index_in_buf + 1] == DOT
+                && self.buffer[self.index_in_buf + 2] == SPACE)
     }
 
     fn get_part_buf(&mut self) -> &[u8] {
-        &self.buffer[self.index..self.index + 32]
+        &self.buffer[self.index_in_buf..self.index_in_buf + 32]
     }
 
     fn set_deleted(&mut self) {
-        self.buffer[self.index] = 0xE5;
+        self.buffer[self.index_in_buf] = DIR_ENTRY_UNUSED;
     }
 
     pub(crate) fn update_item(&mut self, buf: &[u8]) {
@@ -416,55 +440,95 @@ impl<'a> DirIter<'a> {
         if self.is_end_sector() {
             let blank_cluster = self.fat.blank_cluster();
             self.clean_new_cluster_data(blank_cluster);
-            self.fat.write(blank_cluster, 0x0FFFFFFF);
+            // fat.current_cluster != 0
+            self.fat.write(blank_cluster, END_OF_CLUSTER);
             self.fat.write(self.fat.current_cluster, blank_cluster);
-            self.fat.to_previous();
+
+            // TODO 错误处理
+            assert_ne!(self.fat.current_cluster, 0);
+            // 两次 next() 修复 fat.previous_cluster
+            let _ = self.fat.to_previous();
             self.fat.next();
             self.fat.next();
-            self.offset = self.bpb.offset(blank_cluster);
-            self.index = 0;
-            self.sector_offset = 0;
+
+            self.cluster_offset = self.bpb.offset(blank_cluster);
+            self.index_in_buf = 0;
+            self.sector_id_in_cluster = 0;
             self.update_buffer();
         }
-        self.buffer[self.index..self.index + 32].copy_from_slice(buf);
+        self.buffer[self.index_in_buf..self.index_in_buf + 32].copy_from_slice(buf);
     }
 
     pub(crate) fn previous(&mut self) {
-        if self.index == 0 && self.sector_offset != 0 {
-            self.index = BLOCK_SIZE - 32;
-            self.sector_offset -= 1;
+        if self.index_in_buf == 0 && self.sector_id_in_cluster != 0 {
+            self.index_in_buf = BLOCK_SIZE - 32;
+            self.sector_id_in_cluster -= 1;
             self.update_buffer();
-        } else if self.index != 0 {
-            self.index -= 32;
+        } else if self.index_in_buf != 0 {
+            self.index_in_buf -= 32;
         } else {
             let spc = self.bpb.sector_per_cluster_usize();
-            self.sector_offset = spc - 1;
-            self.index = BLOCK_SIZE - 32;
-            self.fat.to_previous();
+            self.sector_id_in_cluster = spc - 1;
+            self.index_in_buf = BLOCK_SIZE - 32;
+
+            // TODO 错误处理
+            assert_ne!(self.fat.current_cluster, 0);
+
+            let _ = self.fat.to_previous();
             self.update_buffer();
         }
     }
 
     pub(crate) fn update_buffer(&mut self) {
         let offset = self.offset_value();
-        self.device
-            .read_blocks(&mut self.buffer, offset, 1)
-            .unwrap();
+        let block_id = offset / BLOCK_SIZE;
+        assert!(offset % BLOCK_SIZE == 0);
+
+        let option = get_block_cache(block_id, Arc::clone(&self.device));
+        if let Some(cache) = option {
+            cache.read().read(0, |cache: &[u8; BLOCK_SIZE]| {
+                self.buffer.copy_from_slice(cache);
+            })
+        } else {
+            self.device
+                .read_blocks(&mut self.buffer, offset, 1)
+                .unwrap();
+        }
     }
 
     pub(crate) fn update(&self) {
-        self.device
-            .write_blocks(&self.buffer, self.offset_value(), 1)
-            .unwrap();
+        let block_id = self.offset_value() / BLOCK_SIZE;
+        assert!(self.offset_value() % BLOCK_SIZE == 0);
+
+        let option = get_block_cache(block_id, Arc::clone(&self.device));
+        if let Some(cache) = option {
+            cache.write().modify(0, |cache: &mut [u8; BLOCK_SIZE]| {
+                cache.copy_from_slice(&self.buffer);
+            })
+        } else {
+            self.device
+                .write_blocks(&self.buffer, self.offset_value(), 1)
+                .unwrap();
+        }
     }
 
     fn clean_new_cluster_data(&self, cluster: u32) {
         let spc = self.bpb.sector_per_cluster_usize();
         for i in 0..spc {
             let offset = self.bpb.offset(cluster) + i * BLOCK_SIZE;
-            self.device
-                .write_blocks(&[0; BLOCK_SIZE], offset, 1)
-                .unwrap();
+            let block_id = offset / BLOCK_SIZE;
+            assert!(offset % BLOCK_SIZE == 0);
+
+            let option = get_block_cache(block_id, Arc::clone(&self.device));
+            if let Some(cache) = option {
+                cache.write().modify(0, |cache: &mut [u8; BLOCK_SIZE]| {
+                    cache.copy_from_slice(&[0; BLOCK_SIZE]);
+                })
+            } else {
+                self.device
+                    .write_blocks(&[0; BLOCK_SIZE], offset, 1)
+                    .unwrap();
+            }
         }
     }
 }
@@ -474,7 +538,7 @@ impl<'a> Iterator for DirIter<'a> {
     type Item = Entry;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.index == 0 {
+        if self.index_in_buf == 0 {
             self.update_buffer();
         }
 
